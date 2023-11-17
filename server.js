e6dba5652be595bb2a4a1f41b7675c8f4001b2bb
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-11-17 12:28:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const faker = require('faker');
 
 const app = express();
 const port = 4000;
@@ -28,7 +29,47 @@ app.get('/user/:username', (req, res) => {
     });
 });
 
-// ... Seus outros endpoints ...
+/// Configurações do Nodemailer (substitua com suas informações de e-mail)
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'seu-email@gmail.com',
+        pass: 'sua-senha'
+    }
+});
+
+app.use(bodyParser.json());
+
+// Rota para lidar com o pedido de redefinição de senha
+app.post('/reset-password', (req, res) => {
+    const userEmail = req.body.email;
+
+    // Verificar se o e-mail está cadastrado (substitua com sua lógica de verificação de e-mail)
+    const userExists = accounts.some(account => account.email === userEmail);
+
+    if (!userExists) {
+        return res.status(404).json({ message: 'E-mail não cadastrado' });
+    }
+
+    // Gerar uma senha aleatória de até 8 dígitos
+    const newPassword = Math.random().toString(36).slice(-8);
+
+    // Enviar e-mail com a nova senha
+    const mailOptions = {
+        from: 'seu-email@gmail.com',
+        to: userEmail,
+        subject: 'Redefinição de Senha',
+        text: `Sua nova senha é: ${newPassword}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ message: 'Erro ao enviar e-mail' });
+        }
+        console.log('E-mail enviado: ' + info.response);
+        return res.status(200).json({ message: 'E-mail enviado com sucesso' });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Servidor está rodando em http://localhost:${port}`);
